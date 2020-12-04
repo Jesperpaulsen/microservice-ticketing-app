ad6b2059db25b0f02d7be7e78f434078d5a5b396
Jesper Paulsen <jesper@jesper.no>
2020-12-04 17:44:59 +0100

    Added auth frontend

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/buildClient";
+import Header from "../components/header";
 
-const app = ({ Component, pageProps }) => {
-  return <Component { ...pageProps } />
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
-export default app;
+AppComponent.getInitialProps = async ({ ctx, Component }) => {
+  const client = buildClient(ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  return { pageProps, currentUser: data.currentUser };
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,15 @@
-const index = () => {
-  return <h1>Landingpage</h1>
-}
+import buildClient from "../api/buildClient";
+
+const index = ({ currentUser }) => {
+  return <h1>You are {currentUser ? "" : "not"} signed in</h1>;
+};
+
+index.getInitialProps = async (context) => {
+  const client = buildClient(context);
+
+  const { data } = await client.get("/api/users/currentUser");
+
+  return data;
+};
 
 export default index;
